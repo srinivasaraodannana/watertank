9c2354fbb7af75672841e18524bf6d248481c94a
DSK9012 <srinivasarao.dannana2000@gmail.com>
2023-02-15 01:40:28 +0530

    hbhj
    khbh

--- bargavi.py
+++ bargavi.py
@@ -1,7 +1,7 @@
  #-------Design of overhead Circular Water Tank(Bhargavi Bilingi----------
 #Library Module Used
 import math 
-import numpy as np
+
 
 
 
@@ -18,7 +18,7 @@ thickness_top_dome =75/1000 #mm
 rise_bottom_top_dome=1000/1000#mm
 thickness_bottom_dome=100/1000#mm
 rise_bottom_dome= 1500/1000 #mm
-bottom_ring_beam= 350/1000 #mm 
+bottom_ring_beam= 350*350/(1000**2) #mm 
 free_board=150/1000 #mm
 height_staging_above_GL=25/1000 #mm
 
@@ -35,26 +35,7 @@ height_staging_above_GL=25/1000 #mm
 
 
 
-#Step-1 
-r1=1+0.0375
-d1=8.5+0.10
-R1=(d1*d1)/(8*r1)+(r1/2)
-DLtm=0.075*25
-LL=0.75
-TL=DLtm+LL
-x1=(d1/(2*R1))
 
-
-
-alpha=np.arcsin(x1)
-print('Thus,semi central  angle alpha =' + alpha +'<51degree and 48minutes')
-print('/nThe entire top dome will remain under hoop compression')
-T1=13.10
-mstress=(T1*1000/(1000*75))
-print('since mstress is less than 5Mpa. it is safe')
-print('since max dimension of the container is less than 15 m,provide 0.24% min reinforcement in each direction in the middle of the dome')
-steelarea=(0.0024*75*1000)
-print('provide 8mm bars @250mm c/c radially and circumferentially ')
 
 
 #Step:-0
@@ -73,14 +54,44 @@ V= (pi/4)*((2*inner_radius_circular_wall)**2)*(height_wall-free_board)-0.05*0.5*
 print("{0} m3 > 125 m3".format(V))
 
 
-# #Step-1 
+#Step-1 Top Dome
+r1=rise_bottom_top_dome+0.0375
+d1=2*R2+0.10
+R1=(d1*d1)/(8*r1)+(r1/2)
+Dead_load_top_dome =(thickness_top_dome)*25
+LL=0.75
+TL=Dead_load_top_dome + LL
+x1=(d1/(2*R1))
+alpha=math.asin(x1)* (180.0 / math.pi)
+alpha=round(alpha, 2)
+print(alpha)
+print('Thus,semi central  angle alpha =' + str(alpha) +'<51degree and 48minutes')
+print('\nThe entire top dome will remain under hoop compression')
+T1=(TL*R1/2)/(1+math.cos(alpha) )
+print(T1)           
+mstress=(T1*1000/(1000*thickness_top_dome*1000))
+print(mstress)
+print('since mstress is less than 5Mpa. it is safe')
+print('since max dimension of the container is less than 15 m,provide 0.24% min reinforcement in each direction in the middle of the dome')
+steelarea=(0.0024*thickness_top_dome*1000*1000)
+print(str(steelarea)+"mm2")
+print('provide 8mm bars @250mm c/c radially and circumferentially ')
 
 
 
 
+# #Step:-2 (Design of Top Ring Beam)
+print("A ring beam is provided at the junction of top dome and the vertical wall to resist hoop tension induced by the top dome. Let its section - 150 mm x 250 mm deep,")
+Hooptension=T1*math.cos(alpha)*D*0.5
+print(str(Hooptension)+"kN")
+print("Permissible stress in steel = 130 Mpa")
+Ast=Hooptension*1000/130
+print(str(Ast)+'Area of steel required')
+print("Provide 4-12 mm hoops and 8 ties @ 250 mm c/c.")
+DirectTensileStressConcrete=(Hooptension*1000)/(250*150+8*(4*113))#have to change this
+print(str(DirectTensileStressConcrete)+"<1.5MPa")
 
-# #Step:-2 (Design of Top Dome)
-# W=1.5+0.1*concrete #assume thickness=100mm, LL=1.5
+
 
 
 
