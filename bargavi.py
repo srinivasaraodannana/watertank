240aad04d76db484ee161e79d7a8e8b8fac1c1e5
DSK9012 <srinivasarao.dannana2000@gmail.com>
2023-02-16 00:33:46 +0530

    xcxc
    xcxcx

--- bargavi.py
+++ bargavi.py
@@ -1,4 +1,4 @@
- #-------Design of overhead Circular Water Tank(Bhargavi Bilingi----------
+ #-------Design of overhead Circular Water Tank(Bhargavi Bilingi)----------
 #Library Module Used
 import math 
 
@@ -28,8 +28,8 @@ height_staging_above_GL=25/1000 #mm
 #Total Designing Steps-5
 #Step-0:- Check for capacity
 #Step-1:- Top Dome
-#Step-2:- Top Ring Beam
-#Step-3:- Tank Wall
+#Step-2:- Vertical wall
+#Step-3:- Bottom Dome
 #Step-4:- Base Slab
 #Step-5:- Bottom Ring Beam
 
@@ -90,6 +90,49 @@ print(str(Ast)+'Area of steel required')
 print("Provide 4-12 mm hoops and 8 ties @ 250 mm c/c.")
 DirectTensileStressConcrete=(Hooptension*1000)/(250*150+8*(4*113))#have to change this
 print(str(DirectTensileStressConcrete)+"<1.5MPa")
+
+
+#Step:-3 Vertical Wall 
+print("Dead load due to top dome = 2 R H Ye")
+H1=1.0375
+R1=R1/2
+Dead_load_top_dome=2*pi*R1*H1*thickness_top_dome*25
+LL=46
+print("Dead load due to top dome",Dead_load_top_dome)
+print(" Live load on top dome = 46 KN")
+Dead_load_top_ring_beam=(thickness_top_dome-height_staging_above_GL)*0.25*25*pi*inner_radius_circular_wall*2 #have to change
+print("Dead load due to top ring beam =",Dead_load_top_ring_beam)
+Dead_load_vertical_wall=inner_radius_circular_wall*2*pi*thickness_bottom_dome*height_wall*25
+print("Dead load due to vertical wall =",Dead_load_vertical_wall)
+Dead_load_taper_wall=inner_radius_circular_wall*2*pi*0.5*(thickness_top_dome-height_staging_above_GL)*rise_bottom_top_dome*25
+print("Dead load due to taper in wall=",Dead_load_taper_wall)
+Tl=Dead_load_top_dome+LL+Dead_load_top_ring_beam+Dead_load_vertical_wall+Dead_load_taper_wall
+print("Total Load=",Tl)
+Tlm=Tl/(pi*inner_radius_circular_wall*2)
+print("Total load per m =",Tlm)
+compression_stress=Tlm*1000/(1000*free_board*1000)
+print("Compressive stress ={0}< 8 MPa".format(compression_stress))
+print(" Hoop tension at any depth x from top = rwhD/2 ")
+##--------------------------------------------------------HAve to draw a table here
+Hooptension=10*3*D/2
+print("At 3m =",Hooptension)
+At=Hooptension*1000/130
+print("Area of steel required to resist hoop tension",At)
+print("Provide 12 hoops @ 100 mm c/c.")
+Ap=1.13*1000/100
+print("Area Provided={0}>981mm2".format(Ap))
+print("Tensile stress in concrete at 3 m depth")
+Tensilestree=Hooptension*1000/(140*1000+8*Ap)
+print("{0} < 1.5 Mpa".format(Tensilestree))
+print("The hoop deel may be curtailed according to hoop tension at different heights along Puride 0.24 % minimum vertical reinforcement, (= 240 mm2/m )that is, 8 mm 150 mm c/c")
+
+#Step:-4 Bottom Dome
+print("If the top surface of the bottom dome makes an angle 2 a at its centre,")
+
+
+
+
+
 
 
 
